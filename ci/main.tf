85fa6c00907ae6b72b300a980aeed1b63f71ee1b
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-23 15:21:39 -0300

    ci test

--- ci/main.tf
+++ ci/main.tf
@@ -24,24 +24,6 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata = {
-    repo_url         = var.repo_url
-
-    db_name          = var.db_name
-    db_user          = var.db_user
-    db_port          = var.db_port 
-    db_schema_name   = var.db_schema_name 
-    db_password      = var.db_password
-
-    jdbc_url         = var.jdbc_url
-    api_port         = var.api_port
-
-    email_smtp       = var.email_smtp 
-    senha_email_smtp = var.senha_email_smtp
-
-    jwt_secret       = var.jwt_secret
-  }
-
   metadata_startup_script = "echo hi > /test.txt"
 
   network_interface {
