e8a8f07a6abcf7e51f901fa1db3b2ff87896d267
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-23 14:03:47 -0300

    metadata startup

--- ci/gcp/terraform/modules/instance/main.tf
+++ ci/gcp/terraform/modules/instance/main.tf
@@ -14,25 +14,45 @@ resource "google_compute_instance" "docker_instance" {
     access_config {}  # IP público
   }
 
+  # metadata = {
+  #   startup-script = templatefile("${path.module}/startup.sh.tpl", {
+  #     repo_url    = var.repo_url
+
+  #     db_name          = var.db_name
+  #     db_user          = var.db_user
+  #     db_port          = var.db_port 
+  #     db_schema_name   = var.db_schema_name 
+  #     db_password      = var.db_password
+
+  #     jdbc_url         = var.jdbc_url
+  #     api_port         = var.api_port
+
+  #     email_smtp       = var.email_smtp 
+  #     senha_email_smtp = var.senha_email_smtp
+
+  #     jwt_secret       = var.jwt_secret
+  #   })
+  # }
+
   metadata = {
-    startup-script = templatefile("${path.module}/startup.sh.tpl", {
-      repo_url    = var.repo_url
+    repo_url         = var.repo_url
 
-      db_name          = var.db_name
-      db_user          = var.db_user
-      db_port          = var.db_port 
-      db_schema_name   = var.db_schema_name 
-      db_password      = var.db_password
+    db_name          = var.db_name
+    db_user          = var.db_user
+    db_port          = var.db_port 
+    db_schema_name   = var.db_schema_name 
+    db_password      = var.db_password
 
-      jdbc_url         = var.jdbc_url
-      api_port         = var.api_port
+    jdbc_url         = var.jdbc_url
+    api_port         = var.api_port
 
-      email_smtp       = var.email_smtp 
-      senha_email_smtp = var.senha_email_smtp
+    email_smtp       = var.email_smtp 
+    senha_email_smtp = var.senha_email_smtp
 
-      jwt_secret       = var.jwt_secret
-    })
+    jwt_secret       = var.jwt_secret
   }
+
+  metadata_startup_script = "apt-get update && apt-get install -y docker.io docker-compose git; systemctl start docker; git clone ${repo_url} /opt/app; cd /opt/app; cd bibliotech/bibliotech; docker-compose up -d --build postgres api"
 
   lifecycle {
     create_before_destroy = true
