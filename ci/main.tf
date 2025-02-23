d4f4cda4dc1ef293add5062df4a359f843d35956
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-23 14:39:43 -0300

    vars

--- ci/main.tf
+++ ci/main.tf
@@ -24,6 +24,24 @@ resource "google_compute_instance" "default" {
     }
   }
 
+  metadata = {
+    repo_url         = var.repo_url
+
+    db_name          = var.db_name
+    db_user          = var.db_user
+    db_port          = var.db_port 
+    db_schema_name   = var.db_schema_name 
+    db_password      = var.db_password
+
+    jdbc_url         = var.jdbc_url
+    api_port         = var.api_port
+
+    email_smtp       = var.email_smtp 
+    senha_email_smtp = var.senha_email_smtp
+
+    jwt_secret       = var.jwt_secret
+  }
+
   metadata_startup_script = "apt-get update && apt-get install -y docker.io docker-compose git; systemctl start docker; git clone https://github.com/Ttito500/acervo-bibliotech.git /opt/app; cd /opt/app; cd bibliotech/bibliotech; docker-compose up -d --build postgres api"
 
   network_interface {
